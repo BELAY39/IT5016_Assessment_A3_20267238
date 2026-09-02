88ee4732a8a2058bb43e62ae842f39a256f149f9
BELAY39 <gebressilassiebelay2@gmail.com>
2026-09-02 18:37:35 +1200

    added to comments

--- code/display_requisitions.py
+++ code/display_requisitions.py
@@ -3,23 +3,25 @@
 #author:Belay Gebressilassie
 #IT5016-Assessment 3,(Student ID:20267238)
 #Class:Software Development
-
+#Global counter used to generate a unique requisition Id
 #Task 1:collect Staff information
 requisition_counter=10000
 def staff_info():
+    #Use the global counter so the requisition ID increases each time
     global requisition_counter
 
-   #ask the user to enter staff details
+   #Collect staff information from the user
     date=input("Enter Date:")
     staff_id=input("Enter Staff ID:")
     staff_name=input("Enter Staff Name:")
 
-   #Generate a unique requisition ID  
+   #Increase the counter to create a new requisition ID
     requisition_counter+=1
     requisition_id=requisition_counter
 
-    #return the staff information and requisition ID
+    #Return all staff information for use by other functions
     return date,staff_id,staff_name,requisition_id
+
 
 #Task 2:Calculate the total cost of requisition items
 def requisitions_total():
@@ -47,7 +49,6 @@ def requisitions_total():
 
    #return the staff details and calculated total
    return date,staff_id,staff_name,requisition_id,total
-
 
 
 #Task 3:Requisition Approval
